565acd3c4c1846e2969d5ae7e021dcff449b17d5
Denys Makarov <denm2000@gmail.com>
2019-12-02 21:56:00 +0200

    Jib Docker images builder added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -29,8 +29,7 @@ podTemplate(
         }
         stage('Build and push docker image') {
             container('maven') {
-                sh 'sudo apt update'
-                sh 'sudo apt install amazon-ecr-credential-helper'
+                sh 'apt-get update update && apt-get install -y amazon-ecr-credential-helper'
                 sh 'mvn compile com.google.cloud.tools:jib-maven-plugin:1.3.0:build'
             }
         }
