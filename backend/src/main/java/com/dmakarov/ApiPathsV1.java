742053314fd00ab79758aaad09074cc282b96d1d
Denys Makarov <denm2000@gmail.com>
2019-12-11 18:53:40 +0200

    New "Hello <name>" endpoint

--- backend/src/main/java/com/dmakarov/ApiPathsV1.java
+++ backend/src/main/java/com/dmakarov/ApiPathsV1.java
@@ -11,4 +11,6 @@ public class ApiPathsV1 {
   public static final String ACCOUNT = "/account";
   public static final String HISTORY = "/history";
 
+  public static final String HELLO = "/hello";
+
 }
