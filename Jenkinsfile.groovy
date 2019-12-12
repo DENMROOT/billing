fbeede6e30db9dda1bd373ba3cd94c189b78e3a0
Denys Makarov <denm2000@gmail.com>
2019-12-12 17:38:49 +0200

    Jenkinsfile container fixes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -70,6 +70,7 @@ podTemplate(
                             sh 'chmod 700 get_helm.sh'
                             sh './get_helm.sh'
 
+                            sh 'mkdir ~/.docker'
                             sh 'cat > ~/.docker/config.json << ENDOFFILE\n' +
                                     '{\n' +
                                     '    "credsStore": "ecr-login"\n' +
