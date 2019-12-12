a7ab3f113099738c54540d8d22fbfb553e718da2
Denys Makarov <denm2000@gmail.com>
2019-12-12 17:27:09 +0200

    Jenkinsfile container fixes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -82,10 +82,8 @@ podTemplate(
                             sh 'chmod 700 get_helm.sh'
                             sh './get_helm.sh'
 
-                            sh 'skaffold run'
+                            sh 'cat ~/.docker/config.json'
                         }
-
-
                     }
                 }
             }
